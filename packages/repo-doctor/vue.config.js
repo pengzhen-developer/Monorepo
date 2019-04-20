610ce5efe31711fbf412358d4c2f9185b8046982
Peng Zhen <peng_zhen@outlook.com>
2019-04-20 18:26:44 +0800

    修改打包位置

--- packages/repo-doctor/vue.config.js
+++ packages/repo-doctor/vue.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // https://cli.vuejs.org/config/#vue-config-js
-  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  publicPath: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ? '' : '/',
 
   // https://cli.vuejs.org/config/#assetsdir
   assetsDir: 'static'
