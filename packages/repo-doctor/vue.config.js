930e17f18624d7618f192a2878e5c4c788f1594a
Peng Zhen <peng_zhen@outlook.com>
2019-04-22 14:07:32 +0800

    Feature 修改全局 scss

--- packages/repo-doctor/vue.config.js
+++ packages/repo-doctor/vue.config.js
@@ -5,5 +5,16 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
 
   // https://cli.vuejs.org/config/#assetsdir
-  assetsDir: process.env.outputDir
+  assetsDir: process.env.outputDir,
+
+  //增加vue.config.js文件配置css
+  css: {
+    loaderOptions: {
+      sass: {
+        // @/ is an alias to src/
+        // so this assumes you have a file named `src/variables.scss`
+        data: `@import "@/assets/css/element-var.scss";`
+      }
+    }
+  }
 }
