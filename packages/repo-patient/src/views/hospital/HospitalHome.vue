bfbfd693d40bb928b9831a7377e8f040b9207659
彭真 <peng_zhen@outlook.com>
2019-10-10 20:10:19 +0800

    Feature

--- packages/repo-patient/src/views/hospital/HospitalHome.vue
+++ packages/repo-patient/src/views/hospital/HospitalHome.vue
@@ -159,7 +159,9 @@ export default {
 
   methods: {
     getHospitalInfo() {
-      const params = peace.util.decode(this.$route.params.json)
+      const params = peace.util.decode(
+        this.$route.params.json || peace.cache.get(peace.type.SYSTEM.PARAMS)
+      )
 
       peace.service.hospital.getHospitalInfo({ nethospitalId: params.netHospitalId }).then(res => {
         this.hospitalInfo = res.data
