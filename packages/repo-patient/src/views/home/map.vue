bc03a8f51806870eb1c106bc0142b34401b5d234
leejee <leejee@qq.com>
2019-11-15 09:50:44 +0800

    地图经纬度查询

--- packages/repo-patient/src/views/home/map.vue
+++ packages/repo-patient/src/views/home/map.vue
@@ -14,32 +14,55 @@ export default {
 
   mounted() {
     const params = peace.util.decode(this.$route.params.json)
-
-    var geo = new window.qq.maps.Geocoder()
-    geo.getLocation(params.address) //地址
-    geo.setComplete(function(res) {
-      console.log(res, res.detail.location) //得到经纬度
-      var map = new window.qq.maps.Map(document.getElementById('map'), {
-        center: res.detail.location, //将经纬度加到center属性上
-        zoom: 13, //缩放
-        draggable: true, //是否可拖拽
-        scrollwheel: true, //是否可滚动缩放
-        disableDoubleClickZoom: false
-      })
-      new window.qq.maps.Marker({
-        position: res.detail.location, //标记的经纬度
-        animation: window.qq.maps.MarkerAnimation.BOUNCE, //标记的动画
-        map: map //标记的地图
-      })
-
-      new window.qq.maps.CityService({
-        //设置地图
-        map: map,
-        complete: function(results) {
-          console.log(9999, results)
-        }
-      })
-    })
+    let center = new window.qq.maps.LatLng(params.latitude, params.longitude);
+    var map = new window.qq.maps.Map(document.getElementById("map"), {
+      // 地图的中心地理坐标。
+      center: center,
+      zoom: 13,
+      draggable: true, //是否可拖拽
+      scrollwheel: true, //是否可滚动缩放
+      disableDoubleClickZoom: false
+    });
+    var marker = new window.qq.maps.Marker({
+      //设置Marker的位置坐标
+      position: center,
+      //设置显示Marker的地图
+      map: map
+    });
+    marker.setVisible(true);
+    // const params = peace.util.decode(this.$route.params.json)
+    //   new window.qq.maps.Marker({
+    //     position: res.detail.location, //标记的经纬度
+    //     animation: window.qq.maps.MarkerAnimation.BOUNCE, //标记的动画
+    //     map: map //标记的地图
+    //   })
+    // const params = peace.util.decode(this.$route.params.json)
+    //
+    // var geo = new window.qq.maps.Geocoder()
+    // geo.getLocation(params.address) //地址
+    // geo.setComplete(function(res) {
+    //   console.log(res, res.detail.location) //得到经纬度
+    //   var map = new window.qq.maps.Map(document.getElementById('map'), {
+    //     center: res.detail.location, //将经纬度加到center属性上
+    //     zoom: 13, //缩放
+    //     draggable: true, //是否可拖拽
+    //     scrollwheel: true, //是否可滚动缩放
+    //     disableDoubleClickZoom: false
+    //   })
+    //   new window.qq.maps.Marker({
+    //     position: res.detail.location, //标记的经纬度
+    //     animation: window.qq.maps.MarkerAnimation.BOUNCE, //标记的动画
+    //     map: map //标记的地图
+    //   })
+    //
+    //   new window.qq.maps.CityService({
+    //     //设置地图
+    //     map: map,
+    //     complete: function(results) {
+    //       console.log(9999, results)
+    //     }
+    //   })
+    // })
   },
   data() {
     return {
