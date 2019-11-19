dadb059e2689cbc4f6c54f3868219438baadca87
leejee <leejee@qq.com>
2019-11-19 10:08:28 +0800

    新增去哪里功能

--- packages/repo-patient/src/views/home/map.vue
+++ packages/repo-patient/src/views/home/map.vue
@@ -1,6 +1,9 @@
 <template>
-  <div id="map"
-       class="map">
+  <div class="map">
+<!--  <div id="map"-->
+<!--       class="map">-->
+<!--  </div>-->
+    <iframe id="mapPage" width="100%" height="100%" frameborder=0 :src="src"></iframe>
   </div>
 </template>
 
@@ -9,65 +12,34 @@ import peace from '@src/library'
 
 export default {
   name: 'home-map',
-
   components: {},
-
-  mounted() {
-    const params = peace.util.decode(this.$route.params.json)
-    let center = new window.qq.maps.LatLng(params.latitude, params.longitude);
-    var map = new window.qq.maps.Map(document.getElementById("map"), {
-      // 地图的中心地理坐标。
-      center: center,
-      zoom: 13,
-      draggable: true, //是否可拖拽
-      scrollwheel: true, //是否可滚动缩放
-      disableDoubleClickZoom: false
-    });
-    var marker = new window.qq.maps.Marker({
-      //设置Marker的位置坐标
-      position: center,
-      //设置显示Marker的地图
-      map: map
-    });
-    marker.setVisible(true);
-    // const params = peace.util.decode(this.$route.params.json)
-    //   new window.qq.maps.Marker({
-    //     position: res.detail.location, //标记的经纬度
-    //     animation: window.qq.maps.MarkerAnimation.BOUNCE, //标记的动画
-    //     map: map //标记的地图
-    //   })
-    // const params = peace.util.decode(this.$route.params.json)
-    //
-    // var geo = new window.qq.maps.Geocoder()
-    // geo.getLocation(params.address) //地址
-    // geo.setComplete(function(res) {
-    //   console.log(res, res.detail.location) //得到经纬度
-    //   var map = new window.qq.maps.Map(document.getElementById('map'), {
-    //     center: res.detail.location, //将经纬度加到center属性上
-    //     zoom: 13, //缩放
-    //     draggable: true, //是否可拖拽
-    //     scrollwheel: true, //是否可滚动缩放
-    //     disableDoubleClickZoom: false
-    //   })
-    //   new window.qq.maps.Marker({
-    //     position: res.detail.location, //标记的经纬度
-    //     animation: window.qq.maps.MarkerAnimation.BOUNCE, //标记的动画
-    //     map: map //标记的地图
-    //   })
-    //
-    //   new window.qq.maps.CityService({
-    //     //设置地图
-    //     map: map,
-    //     complete: function(results) {
-    //       console.log(9999, results)
-    //     }
-    //   })
-    // })
-  },
   data() {
     return {
-      map: {}
+      map: {},
+      src: ""
     }
+  },
+  mounted() {
+    const params = peace.util.decode(this.$route.params.json)
+    console.log(params)
+    this.src = 'https://apis.map.qq.com/tools/poimarker?type=0&marker=coord:' + params.latitude + ',' + params.longitude + ';title:' +  params.name + ';addr: ' + params.address + ';&key=S2WBZ-VHEK5-UCAIE-Q4TPB-LO7P3-DCB54&referer=hospital';
+    console.log(this.src);
+    // let center = new window.qq.maps.LatLng(params.latitude, params.longitude);
+    // var map = new window.qq.maps.Map(document.getElementById("map"), {
+    //   // 地图的中心地理坐标。
+    //   center: center,
+    //   zoom: 13,
+    //   draggable: true, //是否可拖拽
+    //   scrollwheel: true, //是否可滚动缩放
+    //   disableDoubleClickZoom: false
+    // });
+    // var marker = new window.qq.maps.Marker({
+    //   //设置Marker的位置坐标
+    //   position: center,
+    //   //设置显示Marker的地图
+    //   map: map
+    // });
+    // marker.setVisible(true);
   },
   methods: {}
 }
