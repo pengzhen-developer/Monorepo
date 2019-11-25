237dcbe490ec9ea9c95f1eb902dc3a0faf9208ec
bl <bl@bl.com>
2019-11-25 10:38:40 +0800

    Remove console

--- packages/repo-patient/src/views/home/map.vue
+++ packages/repo-patient/src/views/home/map.vue
@@ -24,9 +24,7 @@ export default {
     }
   },
   mounted() {
-    debugger
     const params = peace.util.decode(this.$route.params.json)
-    console.log(params)
     this.src =
       'https://apis.map.qq.com/tools/poimarker?type=0&marker=coord:' +
       params.latitude +
@@ -37,23 +35,6 @@ export default {
       ';addr: ' +
       params.address +
       ';&key=S2WBZ-VHEK5-UCAIE-Q4TPB-LO7P3-DCB54&referer=hospital'
-    console.log(this.src)
-    // let center = new window.qq.maps.LatLng(params.latitude, params.longitude);
-    // var map = new window.qq.maps.Map(document.getElementById("map"), {
-    //   // 地图的中心地理坐标。
-    //   center: center,
-    //   zoom: 13,
-    //   draggable: true, //是否可拖拽
-    //   scrollwheel: true, //是否可滚动缩放
-    //   disableDoubleClickZoom: false
-    // });
-    // var marker = new window.qq.maps.Marker({
-    //   //设置Marker的位置坐标
-    //   position: center,
-    //   //设置显示Marker的地图
-    //   map: map
-    // });
-    // marker.setVisible(true);
   },
   methods: {}
 }
