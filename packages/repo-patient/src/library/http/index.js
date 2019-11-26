430014d642897e29a56abaf1e6586d37882add29
bl <bl@bl.com>
2019-11-26 08:47:35 +0800

    Change warning message

--- packages/repo-patient/src/library/http/index.js
+++ packages/repo-patient/src/library/http/index.js
@@ -16,7 +16,7 @@ import 'nprogress/nprogress.css'
 let httpCount = 0
 
 /** 提醒消息-需要登录 */
-const MESSAGE_USER_NEED_LOGIN = '为保障您的数据安全，请登录后使用。'
+const MESSAGE_USER_NEED_LOGIN = '您的登录信息已过期，请重新登录。'
 
 /** cancel token */
 const cancelTokenList = []
