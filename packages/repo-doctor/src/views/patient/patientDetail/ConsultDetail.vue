4aa07edce5c4845946efaaa480b65de222694c5e
hu <hub>
2019-12-24 16:11:21 +0800

    Fixed:健康档案->会诊、病历->处方详情样式调整

--- packages/repo-doctor/src/views/patient/patientDetail/ConsultDetail.vue
+++ packages/repo-doctor/src/views/patient/patientDetail/ConsultDetail.vue
@@ -126,20 +126,17 @@
               <span class="title">临床诊断：</span>
               <span class="content">{{item.diagnosisInfos}}</span>
             </div>
-
+            <div class="rp-title">Rp</div>
             <div class="dragList">
               <div class="dragItem"
                    v-for="(drug,index) in item.drugCode"
                    :key="'drug' + index">
-                <div class="dragCode">
-                  {{drug.drugCode}}
-                </div>
                 <div class="title">
                   <span>{{drug.drugName}} {{drug.drugSpecifications}}</span>
                   <span> x{{drug.drugQty}}</span>
                 </div>
                 <div class="usage">
-                  用法用量：每日2次，每次50mg，口服，三天
+                  {{drug.drugUse}}
                 </div>
               </div>
             </div>
@@ -169,7 +166,7 @@
               </div>
 
               <div class="line"
-                   style="margin-top: 10px;">
+                   style="margin-top: 5px;border-top: 1px solid #f3f3f3;padding: 5px 0;">
                 <div class="dataItem">
                   <span class="title">药师审方结果：</span>
                   <span class="content">{{getText(item.prescription)}}</span>
@@ -404,6 +401,7 @@ export default {
         display: flex;
         width: 370px;
         margin: 0 auto;
+
         .dataItem {
           width: 170px;
           .title {
@@ -422,12 +420,19 @@ export default {
       }
     }
   }
+  .rp-title {
+    margin-top: 8px;
+    padding-bottom: 4px;
+    font-size: 18px;
+    line-height: 25px;
+    border-bottom: 1px solid #f3f3f3;
+  }
   .dragList {
     .dragItem {
       display: flex;
       flex-direction: column;
       color: #333;
-      height: 90px;
+      height: 60px;
       justify-content: center;
       border-bottom: 1px dashed #c4c4c4;
       .dragCode {
@@ -457,7 +462,6 @@ export default {
     .line {
       display: flex;
       margin-bottom: 5px;
-
       .dataItem {
         width: 250px;
         .title {
