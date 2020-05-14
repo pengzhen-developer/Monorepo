60dd8b0b07246ec42d5635a1a6251bf51d675a83
bl <bl@bl.com>
2020-05-14 09:54:17 +0800

    Change eslint

--- packages/repo-doctor/.eslintrc.js
+++ packages/repo-doctor/.eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
     'no-prototype-builtins': 'off'
   },
   globals: {
