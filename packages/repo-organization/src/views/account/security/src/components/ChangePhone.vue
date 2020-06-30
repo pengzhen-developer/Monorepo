f67bf9aef877c2e78099e785906e5596de34e298
何泰樑 <hetl@jzteyao.com>
2020-06-30 18:49:34 +0800

    完成修改手机号码的UI迁移

--- packages/repo-organization/src/views/account/security/src/components/ChangePhone.vue
+++ packages/repo-organization/src/views/account/security/src/components/ChangePhone.vue
@@ -83,7 +83,7 @@
 export default {
   data() {
     return {
-      showTab1: true,
+      showTab1: false,
       oldPhone: '12312313131',
       oldCode: '',
       newPhone: '213123131',
@@ -100,7 +100,14 @@ export default {
 </script>
 
 <style  scoped>
+p {
+  margin: 0;
+  padding: 0;
+}
 .content-bgd {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   background: white;
