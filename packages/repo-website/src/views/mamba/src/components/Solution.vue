b2944f45ff720c26582b7a11bd0fed60b09b697b
何泰樑 <hetl@jzteyao.com>
2020-07-20 18:00:17 +0800

    解决方案兼容性修复

--- packages/repo-website/src/views/mamba/src/components/Solution.vue
+++ packages/repo-website/src/views/mamba/src/components/Solution.vue
@@ -15,7 +15,7 @@
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
             <img src="../assets/img/my/01.png"
-                 class="img-fluid"
+                 class="img-fluid img"
                  alt="">
             <div class="portfolio-info">
               <h4>互联网医院解决方案</h4>
@@ -26,7 +26,7 @@
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap">
             <img src="../assets/img/my/02.png"
-                 class="img-fluid"
+                 class="img-fluid img"
                  alt="">
             <div class="portfolio-info">
               <h4>医联体解决方案</h4>
@@ -37,7 +37,7 @@
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
             <img src="../assets/img/my/03.png"
-                 class="img-fluid"
+                 class="img-fluid img"
                  alt="">
             <div class="portfolio-info">
               <h4>处方共享解决方案</h4>
@@ -48,7 +48,7 @@
         <div class="col-lg-4 col-md-6 portfolio-item filter-card">
           <div class="portfolio-wrap">
             <img src="../assets/img/my/04.png"
-                 class="img-fluid"
+                 class="img-fluid img"
                  alt="">
             <div class="portfolio-info">
               <h4>合理用药解决方案</h4>
@@ -59,7 +59,7 @@
         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
           <div class="portfolio-wrap">
             <img src="../assets/img/my/05.png"
-                 class="img-fluid"
+                 class="img-fluid img"
                  alt="">
             <div class="portfolio-info">
               <h4>智能审方算法服务</h4>
@@ -71,7 +71,7 @@
         <div class="col-lg-4 col-md-6 portfolio-item filter-app">
           <div class="portfolio-wrap">
             <img src="../assets/img/my/06.png"
-                 class="img-fluid"
+                 class="img-fluid img"
                  alt="">
             <div class="portfolio-info">
               <h4>多店云仓解决方案</h4>
@@ -87,5 +87,8 @@
 <script>
 export default {}
 </script>
-<style >
+<style scoped>
+.img {
+  width: 100%;
+}
 </style>
