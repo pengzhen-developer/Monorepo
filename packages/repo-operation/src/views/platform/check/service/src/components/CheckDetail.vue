59dc48e676918743c730e376da8a81fa4d97266d
xinyuan <1013662753@qq.com>
2020-07-29 12:09:03 +0800

    Feature 修改服务开通（审核、查看详情）

--- packages/repo-operation/src/views/platform/check/service/src/components/CheckDetail.vue
+++ packages/repo-operation/src/views/platform/check/service/src/components/CheckDetail.vue
@@ -1,49 +1,92 @@
 <template>
   <div>
-    <el-dialog width="500px" v-bind:visible.sync="visible" v-bind:title="title">
-      <el-form ref="form" label-width="90px" v-bind:model="model" v-bind:rules="rules">
+    <el-dialog width="500px"
+               v-bind:visible.sync="visible"
+               v-bind:title="title">
+      <el-form ref="form"
+               label-width="90px"
+               v-bind:model="model"
+               v-bind:rules="rules">
         <!-- 待审核 -->
-        <template v-if="data.checkStatus === 1">
-          <el-form-item label="审核结果" prop="checkStatus">
+        <template v-if="data.checkStatus === CONSTANT.ENUM_APPLY_STATUS.待审核">
+          <el-form-item label="机构名称">
+            <span>{{data.hospitalName}}</span>
+          </el-form-item>
+          <el-form-item label="机构类型">
+            <span>{{data.role | getEnumLabel(source.ENUM_ORGANIZATION_TYPE)}}</span>
+          </el-form-item>
+          <el-form-item label="服务名称">
+            <span>{{data.serviceName}}</span>
+          </el-form-item>
+          <el-form-item v-if="data.accessModeStr"
+                        label="对接方式">
+            <span>{{data.accessModeStr}}</span>
+          </el-form-item>
+          <el-form-item v-if="data.serviceContentStr"
+                        label="服务内容">
+            <span>{{data.serviceContentStr}}</span>
+          </el-form-item>
+          <el-form-item label="申请时间">
+            <span>{{data.applyTime}}</span>
+          </el-form-item>
+          <el-form-item label="审核结果"
+                        prop="checkStatus">
             <el-radio-group v-model="model.checkStatus">
               <el-radio :label="3">通过</el-radio>
               <el-radio :label="2">驳回</el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item v-if="model.checkStatus === 2" label="备注" prop="reason">
-            <el-input
-              type="textarea"
-              placeholder="请填写驳回原因"
-              v-bind:rows="4"
-              maxlength="200"
-              show-word-limit
-              v-model="model.reason"
-            ></el-input>
+          <el-form-item v-if="model.checkStatus === 2"
+                        label="备注"
+                        prop="reason">
+            <el-input type="textarea"
+                      placeholder="请填写驳回原因"
+                      v-bind:rows="4"
+                      maxlength="200"
+                      show-word-limit
+                      v-model="model.reason"></el-input>
           </el-form-item>
           <div class="dialog-btn">
+            <el-button type="primary"
+                       v-bind:loading="isLoading"
+                       v-on:click="doCheck">提 交</el-button>
             <el-button v-on:click="visible = false">取 消</el-button>
-            <el-button type="primary" v-bind:loading="isLoading" v-on:click="doCheck">确 定</el-button>
           </div>
         </template>
       </el-form>
 
       <el-form label-width="90px">
-        <!-- 已通过  -->
-        <template v-if="data.checkStatus === 3">
-          <el-form-item label="审核结果">
-            <span>通过</span>
+        <template v-if="data.checkStatus === CONSTANT.ENUM_APPLY_STATUS.已通过 || data.checkStatus === CONSTANT.ENUM_APPLY_STATUS.未通过">
+          <el-form-item label="机构名称">
+            <span>{{data.hospitalName}}</span>
           </el-form-item>
+          <el-form-item label="机构类型">
+            <span>{{data.role | getEnumLabel(source.ENUM_ORGANIZATION_TYPE)}}</span>
+          </el-form-item>
+          <el-form-item label="服务名称">
+            <span>{{data.serviceName}}</span>
+          </el-form-item>
+          <el-form-item v-if="data.accessModeStr"
+                        label="对接方式">
+            <span>{{data.accessModeStr}}</span>
+          </el-form-item>
+          <el-form-item v-if="data.serviceContentStr"
+                        label="服务内容">
+            <span>{{data.serviceContentStr}}</span>
+          </el-form-item>
+          <el-form-item label="申请时间">
+            <span>{{data.applyTime}}</span>
+          </el-form-item>
+          <el-form-item label="审核结果">
+            <span>{{data.checkStatus | getEnumLabel(source.ENUM_APPLY_STATUS)}}</span>
+          </el-form-item>
+          <template v-if="data.checkStatus === CONSTANT.ENUM_APPLY_STATUS.未通过">
+            <el-form-item label="备注">
+              <span>{{ checkInfo.reason }}</span>
+            </el-form-item>
+          </template>
           <el-form-item label="审核时间">
             <span>{{ data.checkTime }}</span>
-          </el-form-item>
-        </template>
-        <!-- 未通过 -->
-        <template v-if="data.checkStatus === 2">
-          <el-form-item label="审核结果">
-            <span>驳回</span>
-          </el-form-item>
-          <el-form-item label="备注">
-            <span>{{ checkInfo.reason }}</span>
           </el-form-item>
         </template>
       </el-form>
@@ -52,9 +95,9 @@
 </template>
 
 <script>
-import CONSTANT from "./../constant";
-import Peace from "@src/library";
-import Service from "./../service";
+import CONSTANT from './../constant'
+import Peace from '@src/library'
+import Service from './../service'
 
 export default {
   props: {
@@ -69,38 +112,43 @@ export default {
   },
 
   filters: {
-    getEnumLabel: function(value, ENUM) {
-      return Object.keys(ENUM).find(key => ENUM[key] === value);
+    getEnumLabel: function (value, ENUM) {
+      return Object.keys(ENUM).find((key) => ENUM[key] === value)
     }
   },
 
   data() {
     let checkStatusEmpty = (rule, value, callback) => {
       if (Peace.validate.isEmpty(value)) {
-        return callback(new Error());
+        return callback(new Error())
       }
-      callback();
-    };
+      callback()
+    }
     let checkReasonEmpty = (rule, value, callback) => {
       if (this.model.checkStatus === 2) {
         if (Peace.validate.isEmpty(value)) {
-          return callback(new Error());
+          return callback(new Error())
         } else {
-          callback();
+          callback()
         }
       } else {
-        callback();
+        callback()
       }
-    };
+    }
     return {
       CONSTANT,
 
       isLoading: false,
       visible: this.value,
 
+      source: {
+        ENUM_ORGANIZATION_TYPE: CONSTANT.ENUM_ORGANIZATION_TYPE,
+        ENUM_APPLY_STATUS: CONSTANT.ENUM_APPLY_STATUS
+      },
+
       model: {
-        checkStatus: "",
-        reason: ""
+        checkStatus: '',
+        reason: ''
       },
 
       checkInfo: {},
@@ -109,35 +157,35 @@ export default {
         checkStatus: [
           {
             validator: checkStatusEmpty,
-            message: "请选择审核结果",
-            trigger: "change"
+            message: '请选择审核结果',
+            trigger: 'change'
           }
         ],
         reason: [
           {
             validator: checkReasonEmpty,
-            message: "请填写驳回原因",
-            trigger: "blur"
+            message: '请填写驳回原因',
+            trigger: 'blur'
           }
         ]
       }
-    };
+    }
   },
 
   watch: {
     value(value) {
-      this.visible = value;
+      this.visible = value
     },
 
     visible(value) {
-      this.$emit("input", value);
+      this.$emit('input', value)
 
       this.$nextTick(() => {
-        this.$refs.form.resetFields();
-      });
+        this.$refs.form.resetFields()
+      })
 
-      if (this.data.checkStatus === 2) {
-        this.getReason();
+      if (this.data.checkStatus === CONSTANT.ENUM_APPLY_STATUS.未通过) {
+        this.getReason()
       }
     }
   },
@@ -145,57 +193,58 @@ export default {
   methods: {
     doCheck() {
       this.validateForm().then(() => {
-        this.isLoading = true;
+        this.isLoading = true
 
-        const params = Peace.util.deepClone(this.model);
-        params.accountServiceId = this.data.id;
+        const params = Peace.util.deepClone(this.model)
+        params.accountServiceId = this.data.id
 
         Service.doCheck(params)
-          .then(res => {
-            Peace.util.success(res.msg);
-            this.visible = false;
-            this.$emit("refresh");
-            this.isLoading = false;
+          .then((res) => {
+            Peace.util.success(res.msg)
+            this.visible = false
+            this.$emit('refresh')
+            this.isLoading = false
           })
           .catch(() => {
-            this.isLoading = false;
-          });
-      });
+            this.isLoading = false
+          })
+      })
     },
 
     validateForm() {
-      return new Promise(resolve => {
-        this.$refs.form.validate(valid => {
+      return new Promise((resolve) => {
+        this.$refs.form.validate((valid) => {
           if (valid) {
-            resolve();
+            resolve()
           }
-        });
-      });
+        })
+      })
     },
 
     getReason() {
       const params = {
         accountServiceId: this.data.id
-      };
+      }
 
       Service.getReason(params)
-        .then(res => {
-          this.checkInfo = res.data;
+        .then((res) => {
+          this.checkInfo = res.data
         })
-        .finally(() => {});
+        .finally(() => {})
     }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
 ::v-deep .el-form-item__label {
   position: relative;
   padding-right: 30px;
+  color: #333333;
   text-align: justify;
   text-align-last: justify;
   &::after {
-    content: "：";
+    content: '：';
     position: absolute;
     top: 0;
     right: 18px;
@@ -204,8 +253,12 @@ export default {
 
 .el-form-item__content {
   span {
-    color: #9da4af;
+    color: #333333;
   }
+}
+
+.el-radio {
+  color: #333333;
 }
 
 .el-form-item {
