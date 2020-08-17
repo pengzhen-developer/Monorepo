ac3704c0d83e963dafe5658970911af2ec1feb83
yuegu <675473760@qq.com>
2020-08-17 16:38:13 +0800

    Fixed 预售订单 - 优惠金额添加单位

--- packages/repo-patient/src/views/drug/DrugOrderBefore.vue
+++ packages/repo-patient/src/views/drug/DrugOrderBefore.vue
@@ -110,7 +110,7 @@
         <div class="dl-packet">
           <div class="dt">优惠金额 ：</div>
           <div class="dd"
-               :class="PromotionsCut>0&&'money'">{{PromotionsCut}}</div>
+               :class="PromotionsCut>0&&'money'">-￥{{PromotionsCut}}</div>
         </div>
         <!-- <div class="line"></div>
         <div class="dl-packet">
