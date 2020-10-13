e3021f5712e6980de162344c3429a9d3aaab837f
bl <bl@bl.com>
2020-10-13 17:10:19 +0800

    Change LayoutHeader Style

--- packages/repo-organization/src/layouts/default/src/components/LayoutHeader.vue
+++ packages/repo-organization/src/layouts/default/src/components/LayoutHeader.vue
@@ -2,28 +2,34 @@
   <q-toolbar class="q-px-none"
              style="height: 80px;">
     <div class="full-width full-height flex justify-between items-center bg-primary">
-      <div class="full-height flex items-center justify-center">
-        <div style="width: 240px; background: #2c8d98; box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.1);"
-             class="full-height flex items-center justify-center">
-          <q-img class="cursor-pointer"
-                 style="width: 160px;"
-                 v-bind:src="require('@src/assets/logo.png')"
-                 v-on:click="goWebSite"></q-img>
-        </div>
+      <div style="width: 240px; background: #2c8d98; box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.1);"
+           class="full-height flex column items-center justify-center">
+        <q-img class="cursor-pointer inline"
+               style="width: 140px; height: 40px;"
+               contain
+               v-bind:src="require('@src/assets/logo.png')"></q-img>
 
         <transition enter-active-class="animated jump-out"
                     leave-active-class="animated jump-up">
-          <div class="q-ml-lg flex column justify-center flex-start animated jump-down">
-            <div class="text-h6">
-              {{ configuration.application.title }}
-            </div>
-            <div class="text-subtitle2">
-              {{ user.hospitalName }}
-            </div>
-          </div>
+          <span class="gt-xl inline text-subtitle1 animated jump-down"
+                style="min-width: 140px;
+                         display: inline-block;
+                         text-align: justify;
+                         text-align-last: justify;">
+            {{ configuration.application.title }}
+          </span>
         </transition>
-
       </div>
+
+      <transition enter-active-class="animated jump-out"
+                  leave-active-class="animated jump-up">
+        <div class="col q-ml-lg flex column justify-center flex-start animated jump-down">
+
+          <div class="text-h6">
+            {{ user.hospitalName }}
+          </div>
+        </div>
+      </transition>
 
       <div class="flex items-center">
 
