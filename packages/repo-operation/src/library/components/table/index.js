2505e4d966f9d3cde6af84081a14676c95ab75a4
yuegu <675473760@qq.com>
2020-10-14 14:29:14 +0800

    移除本library

--- packages/repo-operation/src/library/components/table/index.js
+++
@@ -1,9 +0,0 @@
-import Table from './src/table'
-import TableColumn from './src/table-column'
-import Pagination from './src/pagination'
-
-export { Table }
-export { TableColumn }
-export { Pagination }
-
-export default { Table, TableColumn, Pagination }
