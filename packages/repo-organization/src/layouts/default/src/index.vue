dd73d19748407352bb45ade1f23201211c122c07
yuegu <675473760@qq.com>
2020-10-21 18:39:34 +0800

    【ID1001688】 【 智药云机构端药品供应管理】云仓为接入系统的机构，仍然会显示出药房接单菜单

--- packages/repo-organization/src/layouts/default/src/index.vue
+++ packages/repo-organization/src/layouts/default/src/index.vue
@@ -127,8 +127,7 @@ export default {
         系统对接: '',
         非系统对接: '药房接单'
       }
-      console.log(Util.user.getUserInfo())
-      const accountMenuList = Util.user.getAccountMenuList()
+      const accountMenuList = Util.user.getUserInfo().accountMenuList
       const menuBefore = []
       accountMenuList.map((menu) => {
         if (!menu.status && menu.title) {
