4b8a08b79aa6f8c20a0b1ad71c9cf6459915c72f
bl <bl@bl.com>
2020-11-02 16:52:20 +0800

    Upgrade dependencies

--- packages/repo-auth/src/boot/boot_identity.js
+++ packages/repo-auth/src/boot/boot_identity.js
@@ -1,20 +1,18 @@
 // import library plugin
-import { version, name } from '@/package'
 
-import Peace from '@jk998/library'
+import Identity from '@jk998/identity'
 
 // export boot install function
 // async is optional
 export default async ({ Vue }) => {
-  Vue.use(Peace, {
-    config: {
-      appName: name,
-      appVersion: version
+  Vue.use(Identity, {
+    auth: {
+      baseApi: 'https://devauth.imedcloud.jk.com/'
     }
   })
 
   console.log(
-    `%c ${'Library'} %c ${Peace.version} %c`,
+    `%c ${'Library'} %c ${Identity.version} %c`,
     'background:#35495e ; padding: 1px; border-radius: 3px;  color: #fff',
     'background:#41b883 ; padding: 1px; border-radius: 3px;  color: #fff',
     'background:transparent'
