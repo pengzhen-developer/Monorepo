4a1f991a1780090777d3d44e95a4b30bd23992be
bl <bl@bl.com>
2020-11-04 15:43:44 +0800

    Change redirectToPath

--- packages/repo-operation/src/util/modules/location.js
+++ packages/repo-operation/src/util/modules/location.js
@@ -1,13 +1,21 @@
 /**
  * 重定向到 path
  *
- * @param {string} [referer=''] 重定向地址
+ * @param {string} URL 可选。打开指定的页面的URL。如果没有指定URL，打开一个新的空白窗口
+ * @param {string} type 可选。指定target属性或窗口的名称。支持以下值：
+                              _blank - URL加载到一个新的窗口。这是默认
+                              _parent - URL加载到父框架
+                              _self - URL替换当前页面
+                              _top - URL替换任何可加载的框架集
+                              name - 窗口名称
+ * @param {string} specs 可选。一个逗号分隔的项目列表。
+ * @param {string} replace 可选。Optional.Specifies规定了装载到窗口的 URL 是在窗口的浏览历史中创建一个新条目，还是替换浏览历史中的当前条目。支持下面的值：
+                                true - URL 替换浏览历史中的当前条目。
+                                false - URL 在浏览历史中创建新的条目。
  * @returns
  */
-export const redirectToPath = (path) => {
-  if (Peace.validate.isUrl(path)) {
-    window.location.href = path
-  }
+export const redirectToPath = (URL, name = '_self', specs, replace) => {
+  window.open(URL, name, specs, replace)
 }
 
 /**
