f05646ebe96568dc1ca997d434e0a73ab7806c4e
yuegu <675473760@qq.com>
2020-12-02 14:03:36 +0800

    合理用药菜单调整

---
+++ packages/repo-organization/src/views/basic_data/rule-dictionary/constant.js
@@ -0,0 +1,16 @@
+export default {
+  /**
+   * 规则字典三级菜单
+   * */
+
+  menuList: [
+    { id: '0', title: 'TNA药品', url: '/Engine/Web?uri=dictionary/organDictionary/TANDrugList/TANDrugList.html' },
+    { id: '1', title: '检验维护', url: '/Engine/Web?uri=dictionary/organDictionary/checkItemList/checkItemList.html' },
+    { id: '2', title: '药品包装规格', url: '/CustDrug/CustDrug' },
+    { id: '3', title: '单位转换', url: '/CustDrug/UnitSwitch' },
+    { id: '4', title: '过敏维护', url: '/Engine/Web?uri=dictionary/organDictionary/allergyInfoList/allergyInfoDataList.html' },
+    { id: '5', title: '人群维护', url: '/Engine/Web?uri=dictionary/organDictionary/humanClassifyList/humanClassifyDataList.html' },
+    { id: '6', title: '年龄维护', url: '/Engine/Web?uri=dictionary/organDictionary/ageInfoList/ageInfoDataList.html' },
+    { id: '7', title: '诊断维护', url: '/Engine/Web?uri=dictionary/organDictionary/diagnosisList/diagnosisDataList.html' }
+  ]
+}
