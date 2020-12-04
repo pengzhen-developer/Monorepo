96902e4122d4bb85f0e8afc5689c63220f3b2dd1
yuegu <675473760@qq.com>
2020-12-04 11:31:03 +0800

    Fixed 详情states字段修改

--- packages/repo-patient/src/views/setting/order/userDrugDetail.vue
+++ packages/repo-patient/src/views/setting/order/userDrugDetail.vue
@@ -520,7 +520,7 @@ export default {
         if (this.order.purchaseDrugOrderStreams.length > 0) {
           this.order.purchaseDrugOrderStreams.map((item, index) => {
             /**拼接 运单编号 */
-            if (item.ServiceStates == 3 && this.showTrackingNumber) {
+            if (item.states == 3 && this.showTrackingNumber) {
               return this.order.purchaseDrugOrderStreams.splice(index, 1, item, { createdTime: this.order.expressNo, states: 9, timeStatusTxt: '运单编号' })
             }
           })
