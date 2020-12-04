90f6472cee4ccf91a16e847b939b758f3516d340
146153 <491304332@qq.com>
2020-12-04 10:24:28 +0800

    服务概览

--- packages/repo-pharmacist/src/views/home/dashboard/src/index.vue
+++ packages/repo-pharmacist/src/views/home/dashboard/src/index.vue
@@ -1,27 +1,64 @@
 <template>
-  <PeaceIframe full
-               v-bind:src="src"></PeaceIframe>
+  <div class="layout-route bg-grey-2"
+       v-if="load">
+    <!-- 未审处方、已审处方 -->
+    <PrescripStatistic v-bind:data="prescripStatis"></PrescripStatistic>
+
+    <div class="row q-gutter-md q-mb-20">
+      <!-- 处方审核情况 -->
+      <div class="col">
+        <PrescripReview v-bind:data="model"></PrescripReview>
+      </div>
+      <!-- 系统审方统计 -->
+      <div class="col">
+        <SystemReview v-bind:data="model"></SystemReview>
+      </div>
+    </div>
+    <!-- 规则触发统计 -->
+    <RuleTrigger v-bind:data="model"></RuleTrigger>
+  </div>
 </template>
 
 <script>
-import PeaceIframe from '@src/views/peace-iframe'
+import Service from './service'
+import PrescripStatistic from './components/PrescripStatistic'
+import PrescripReview from './components/PrescripReview'
+import SystemReview from './components/SystemReview'
+import RuleTrigger from './components/RuleTrigger'
 
 export default {
   name: 'Dashboard',
 
   components: {
-    PeaceIframe
+    PrescripStatistic,
+    PrescripReview,
+    SystemReview,
+    RuleTrigger
   },
 
   data() {
     return {
-      src: ''
+      load: false,
+      prescripStatis: {
+        checkedCount: 0,
+        uncheckedCount: 0
+      },
+      model: {
+        hosCode: '',
+        phaUserId: 0
+      }
     }
   },
 
-  created() {
-    Peace.identity.auth.getAuth().then((res) => {
-      this.src = `${process.env.VUE_APP_SITE_PHARMACIST}Control/IndexPage?token=${res.access_token}`
+  async created() {
+    const userinfo = await Peace.identity.auth.getAccountInfo()
+    this.model.hosCode = userinfo.organCode
+    this.model.phaUserId = userinfo.id
+    this.load = true
+    const params = Peace.util.deepClone(this.model)
+    Service.phaPreCountsByCust(params).then((res) => {
+      this.prescripStatis.checkedCount = Peace.numeral(res.data.list.checkedCount).format('0,0')
+      this.prescripStatis.uncheckedCount = Peace.numeral(res.data.list.uncheckedCount).format('0,0')
     })
   }
 }
---
+++ packages/repo-pharmacist/src/views/home/dashboard/src/service/index.js
@@ -0,0 +1,78 @@
+export default {
+  /**
+   * 处方审核情况
+   *
+   * @param {*} params
+   * @returns {Object}
+   */
+  prescriptionExeStatisticsChart(params) {
+    const isMock = false
+
+    const apiPath = 'prescription/statisticalAnalysis/prescriptionExeStatisticsChart'
+    const mockPath = process.env.VUE_APP_API_MOCK + apiPath
+    const serverPath = process.env.VUE_APP_API_BASE + apiPath
+
+    const requestApi = isMock ? mockPath : serverPath
+
+    return Peace.http.post(requestApi, params).then((res) => {
+      return res
+    })
+  },
+  /**
+   * 系统审方统计
+   *
+   * @param {*} params
+   * @returns {Object}
+   */
+  systemExeStatisticsChart(params) {
+    const isMock = false
+
+    const apiPath = 'prescription/statisticalAnalysis/systemExeStatisticsChart'
+    const mockPath = process.env.VUE_APP_API_MOCK + apiPath
+    const serverPath = process.env.VUE_APP_API_BASE + apiPath
+
+    const requestApi = isMock ? mockPath : serverPath
+
+    return Peace.http.post(requestApi, params).then((res) => {
+      return res
+    })
+  },
+  /**
+   * 规则触发统计
+   *
+   * @param {*} params
+   * @returns {Object}
+   */
+  ruleTriggerStatisticsChart(params) {
+    const isMock = false
+
+    const apiPath = 'prescription/statisticalAnalysis/ruleTriggerStatisticsChart'
+    const mockPath = process.env.VUE_APP_API_MOCK + apiPath
+    const serverPath = process.env.VUE_APP_API_BASE + apiPath
+
+    const requestApi = isMock ? mockPath : serverPath
+
+    return Peace.http.post(requestApi, params).then((res) => {
+      return res
+    })
+  },
+  /**
+   *处方数据(未审核总数和已审核总数)
+   *
+   * @param {*} params
+   * @returns {Object}
+   */
+  phaPreCountsByCust(params) {
+    const isMock = false
+
+    const apiPath = `prescription/data/phaPreCountsByCust?hosCode=${params.hosCode}&phaUserId=${params.phaUserId}`
+    const mockPath = process.env.VUE_APP_API_MOCK + apiPath
+    const serverPath = process.env.VUE_APP_API_BASE + apiPath
+
+    const requestApi = isMock ? mockPath : serverPath
+
+    return Peace.http.get(requestApi).then((res) => {
+      return res
+    })
+  }
+}
