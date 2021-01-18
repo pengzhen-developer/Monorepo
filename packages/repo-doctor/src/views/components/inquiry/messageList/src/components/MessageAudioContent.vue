38ab540140f00ffe601b6b7b255720c937ed836d
yuegu <675473760@qq.com>
2021-01-18 16:43:01 +0800

    语音消息单位去s

--- packages/repo-doctor/src/views/components/inquiry/messageList/src/components/MessageAudioContent.vue
+++ packages/repo-doctor/src/views/components/inquiry/messageList/src/components/MessageAudioContent.vue
@@ -47,7 +47,7 @@ export default {
     getMessageText() {
       if (this.message?.content?.dur >= 0 || this.message?.file?.dur >= 0) {
         const dur = this.message?.content?.dur || this.message?.file?.dur
-        return Math.ceil(this.formatDur(dur) / 1000) + 's"'
+        return Math.ceil(this.formatDur(dur) / 1000) + '”'
       }
     },
     getAudioWidth() {
