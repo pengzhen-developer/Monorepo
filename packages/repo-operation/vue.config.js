b6f48c6d1370ce1385107be6d65e00f3b9f66604
yuegu <675473760@qq.com>
2021-01-28 15:02:33 +0800

    Add Delete preload

--- packages/repo-operation/vue.config.js
+++ packages/repo-operation/vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
   chainWebpack: (config) => {
     // 移除 prefetch 插件
     config.plugins.delete('prefetch')
-
+    config.plugins.delete('preload')
     // 设置别名
     config.resolve.alias
       .set('@', path.join(__dirname, ''))
