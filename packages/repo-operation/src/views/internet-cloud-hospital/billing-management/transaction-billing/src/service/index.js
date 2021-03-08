e4989d5917bceb499248911e53ced735d322a58f
yuegu <675473760@qq.com>
2021-03-08 11:24:26 +0800

    ZHYY-1925

--- packages/repo-operation/src/views/internet-cloud-hospital/billing-management/transaction-billing/src/service/index.js
+++ packages/repo-operation/src/views/internet-cloud-hospital/billing-management/transaction-billing/src/service/index.js
@@ -74,5 +74,22 @@ export default {
     return Peace.http.get(requestApi, { params }).then((res) => {
       return res
     })
+  },
+  /**
+   * 导出
+   *
+   * @param {*} params
+   * @returns {Object}
+   */
+  exportBillsList(params) {
+    const isMock = false
+
+    const apiPath = 'nethospital/operate/bills/export'
+    const mockPath = process.env.VUE_APP_API_MOCK + apiPath
+    const serverPath = process.env.VUE_APP_API_BASE + apiPath
+
+    const requestApi = isMock ? mockPath : serverPath
+
+    return Peace.http.download(requestApi, params, 'post')
   }
 }
