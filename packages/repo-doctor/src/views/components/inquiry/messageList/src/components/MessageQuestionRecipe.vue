4b0ea0343385c67b0eac5bff1add61a73c892601
bl <bl@bl.com>
2021-05-16 15:16:41 +0800

    fix:医患聊天页面质疑处方提示-立即查看按钮不正确
    https://jira.yyjzt.com/browse/ZHYY-3379

--- packages/repo-doctor/src/views/components/inquiry/messageList/src/components/MessageQuestionRecipe.vue
+++ packages/repo-doctor/src/views/components/inquiry/messageList/src/components/MessageQuestionRecipe.vue
@@ -3,7 +3,7 @@
   <div class="message-body"
        style="padding: 3px 20px;">
     <div @click="onClickText()"
-         class="text-grey-999">您所开处方被质疑，请尽快处理，<span class="text-primary">立即查看处方</span></div>
+         class="text-grey-999">您所开处方被质疑，请尽快处理，<span class="text-primary">立即查看</span>处方</div>
   </div>
 </template>
 
