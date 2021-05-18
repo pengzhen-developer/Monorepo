0967fbb82a2ef09fd80b079dc17812a17a45712e
xinyuan <1013662753@qq.com>
2021-05-18 16:32:25 +0800

    fix: 更新获取声音接口返回值

--- packages/repo-pharmacist/src/layouts/side/src/components/LayoutNotifyPrescription.vue
+++ packages/repo-pharmacist/src/layouts/side/src/components/LayoutNotifyPrescription.vue
@@ -48,7 +48,7 @@ export default {
   methods: {
     getOrderVoiceRemind() {
       return Service.getVoiceRemind({ userId: this.userId }).then((res) => {
-        this.orderVoiceRemind = Number(res.data)
+        this.orderVoiceRemind = Number(res.data?.voice)
       })
     },
 
