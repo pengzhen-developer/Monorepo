ee33b5bf54ff6829b2c607dedce41722a4ab5c31
146153 <491304332@qq.com>
2021-06-18 09:14:50 +0800

    feat:新增订单类型

--- packages/repo-operation/src/views/internet-cloud-hospital/separate-accounts/receiver/src/contant.js
+++ packages/repo-operation/src/views/internet-cloud-hospital/separate-accounts/receiver/src/contant.js
@@ -8,6 +8,8 @@ export default {
     { value: 1, label: '咨询订单' },
     { value: 2, label: '复诊订单' },
     { value: 3, label: '购药订单' },
-    { value: 5, label: '服务包订单' }
+    { value: 5, label: '服务包订单' },
+    { value: 6, label: '检验挂号订单' },
+    { value: 7, label: '检验单订单' }
   ]
 }
