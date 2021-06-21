55e1d1be5bd6074ad6754ca79dc5632fa2868ba6
何泰樑 <hetl@jzteyao.com>
2021-06-21 15:57:08 +0800

    feat-基础字典-全国医院字典功能迁移2

--- packages/repo-operation/src/views/internet-cloud-hospital/platform-management/base-dictionary/national-hospital-dictionary/src/components/HospitalInfo.vue
+++ packages/repo-operation/src/views/internet-cloud-hospital/platform-management/base-dictionary/national-hospital-dictionary/src/components/HospitalInfo.vue
@@ -1,149 +1,155 @@
 <template>
-  <div>
-    <div class="el-dialog__body">
-      <el-form ref="model"
-               label-position="right"
-               label-width="95px"
-               label-suffix="："
-               v-on:submit.native.prevent
-               v-on:keyup.enter.native="save('model')"
-               v-bind:model="model"
-               v-bind:rules="rules">
-        <el-form-item label="医院编码">
-          <PeaceInput v-model.trim="model.code"
-                      :disabled="true"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="医院名称"
-                      prop="name">
-          <PeaceInput v-model.trim="model.name"
-                      placeholder="请输入"
-                      maxlength="50"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="简介">
-          <PeaceInput :autosize="{ minRows: 3 }"
-                      v-model.trim="model.content"
-                      placeholder="请输入"
-                      type="textarea"
-                      maxlength="200"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="地址">
-          <PeaceInput v-model.trim="model.address"
-                      placeholder="请输入"
-                      maxlength="50"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="联系电话">
-          <PeaceInput v-model.trim="model.phone"
-                      placeholder="请输入"
-                      maxlength="50"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="网站地址">
-          <PeaceInput v-model.trim="model.web_site"
-                      placeholder="请输入"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="所属区域">
-          <PeaceInput v-model.trim="model.area"
-                      placeholder="请输入"
-                      maxlength="50"></PeaceInput>
-        </el-form-item>
-        <el-form-item label="是否展示">
-          <el-radio-group v-model="model.is_show">
-            <el-radio :label="1">展示</el-radio>
-            <el-radio :label="2">不展示</el-radio>
-          </el-radio-group>
-        </el-form-item>
-
-      </el-form>
+  <div class="info">
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>医院编码</span>
+      </div>
+      <div class="info-row-content">
+        <el-input disabled
+                  readonly
+                  v-model="detail.code"></el-input>
+      </div>
     </div>
-    <div class="el-dialog__footer">
-      <el-button v-on:click="onCancel">取消</el-button>
-      <el-button type="primary"
-                 v-on:click="save('model')">保存</el-button>
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>医院名称</span>
+      </div>
+      <div class="info-row-content">
+        <el-input @blur="update"
+                  v-model="detail.name"></el-input>
+      </div>
+    </div>
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>简介</span>
+      </div>
+      <div class="info-row-content">
+        <el-input :autosize="{ minRows: 3 }"
+                  @blur="update"
+                  resize="none"
+                  type="textarea"
+                  v-model="detail.content"></el-input>
+      </div>
+    </div>
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>地址</span>
+      </div>
+      <div class="info-row-content">
+        <el-input @blur="update"
+                  v-model="detail.address"></el-input>
+      </div>
+    </div>
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>联系电话</span>
+      </div>
+      <div class="info-row-content">
+        <el-input @blur="update"
+                  v-model="detail.phone"></el-input>
+      </div>
+    </div>
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>网站地址</span>
+      </div>
+      <div class="info-row-content">
+        <el-input @blur="update"
+                  v-model="detail.web_site"></el-input>
+      </div>
+    </div>
+    <div class="info-row">
+      <div class="info-row-label">
+        <span>所属区域</span>
+      </div>
+      <div class="info-row-content">
+        <el-input @blur="update"
+                  v-model="detail.area"></el-input>
+      </div>
+    </div>
+    <div class="inline-center mb-sm mt-lg">
+      <el-radio-group v-model="detail.is_show">
+        <el-radio :label="1">展示</el-radio>
+        <el-radio :label="2">不展示</el-radio>
+      </el-radio-group>
     </div>
   </div>
-
 </template>
-
 <script>
 import Service from '../service/index'
 
 export default {
-  name: 'HospitalInfo',
+  name: 'infoInfo',
   props: {
-    data: Object
+    info: Object
   },
   watch: {
-    data: {
-      handler() {
-        this.model.hospitalId = this.data.id
-      },
-      immediate: true
+    'detail.is_show'() {
+      this.update()
     }
   },
   data() {
     return {
-      model: {
-        address: '',
-        area: '',
-        content: '',
-        hospitalName: '',
-        phone: '',
-        show: '',
-        webSite: '',
-        hospitalId: ''
-      },
-      rules: {
-        name: [
-          {
-            required: true,
-            message: '医院名称必填，请输入',
-            trigger: 'blur'
-          }
-        ]
-      }
+      id: this.info.id,
+      detail: Object.assign({}, this.info)
     }
   },
-
-  async mounted() {
-    this.$nextTick().then(() => {
-      this.detailHospital()
-    })
-  },
-
   methods: {
-    detailHospital() {
-      const params = { hospital_id: this.model.hospitalId }
-      Service.detailHospital(params).then((res) => {
-        this.model = res.data
-      })
-    },
-    onCancel() {
-      this.$emit('onClose', {})
-    },
-    save(model) {
-      this.$refs[model].validate((valid) => {
-        if (valid) {
-          const params = {
-            address: this.model.address,
-            area: this.model.area,
-            content: this.model.content,
-            hospitalId: this.data.id,
-            hospitalName: this.model.name,
-            phone: this.model.phone,
-            show: this.model.is_show,
-            webSite: this.model.web_site
-          }
-          Service.editHospital(params).then((res) => {
-            Peace.util.success(res.msg)
+    // 更新数据
+    update() {
+      const isChange = !this.compared(this.detail, this._info)
+      if (isChange) {
+        // 发生了改变
+        const info = this.detail
+        this._info = Object.assign({}, info)
+        const { content, address, phone, area, is_show } = info
+
+        const params = {
+          hospitalId: this.id,
+          hospitalName: info.name,
+          content,
+          address,
+          phone,
+          webSite: info.web_site,
+          area,
+          show: is_show
+        }
+
+        Service.editHospital(params)
+          .then(() => {
+            Peace.util.success('信息已自动保存成功！')
             this.$emit('onSuccess')
           })
-        } else {
-          return false
-        }
+          .catch(() => {
+            this.$emit('close')
+          })
+      }
+    },
+    compared(target, _target) {
+      let flag = true
+
+      Object.keys(target).forEach((key) => {
+        if (target[key] !== _target[key]) flag = false
       })
+
+      return flag
     }
+  },
+  created() {
+    this._info = Object.assign(this.info)
   }
 }
 </script>
-  
-<style>
+<style lang="scss" scoped>
+::v-deep .info-row {
+  margin-bottom: 10px;
+  &-label {
+    &:after {
+      display: none;
+    }
+  }
+  &-content {
+    padding-left: 20px;
+  }
+}
 </style>
