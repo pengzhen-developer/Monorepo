8622fdc439e77674222ad696e54fa297e85cf7b7
146153 <491304332@qq.com>
2021-06-28 09:37:30 +0800

    feat:去掉重复字典

--- packages/repo-operation/src/views/internet-cloud-hospital/advertising-management/advertising/src/constant.js
+++ packages/repo-operation/src/views/internet-cloud-hospital/advertising-management/advertising/src/constant.js
@@ -4,23 +4,10 @@ export default {
     { value: 2, label: '医生APP首页' },
     { value: 3, label: '患者端小程序首页' }
   ],
-  // 服务类型（私人医生）
-  AdvertisementFeature: [
-    { value: 1, label: '周卡/7天' },
-    { value: 2, label: '月卡/30天' },
-    { value: 3, label: '季卡/90天' },
-    { value: 4, label: '半年卡/180天' },
-    { value: 5, label: '年卡/365天' }
-  ],
   // 科室属性
   DisplayAttrForDepartment: [
     { value: 0, label: '展示' },
     { value: 1, label: '不展示' }
-  ],
-  // 广告展示位置
-  AdvertisementPosition: [
-    { value: 2, label: '医生APP首页' },
-    { value: 3, label: '患者端小程序首页' }
   ],
   // 广告功能
   AdvertisementFeature: [
