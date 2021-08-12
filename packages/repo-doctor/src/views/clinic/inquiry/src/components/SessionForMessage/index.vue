e9e9ddae88e7332f1f7ee9b35452088e2ad1daf1
Peng Zhen <pengzhen@jk.com>
2021-08-12 14:31:35 +0800

    fix:add cache key

--- packages/repo-doctor/src/views/clinic/inquiry/src/components/SessionForMessage/index.vue
+++ packages/repo-doctor/src/views/clinic/inquiry/src/components/SessionForMessage/index.vue
@@ -38,6 +38,7 @@
                      v-bind:thumb-style="thumbStyle">
         <Component v-bind:is="componentInstance"
                    v-bind:session="session"
+                   v-bind:cacheKey="session.content.inquiryInfo.inquiryNo"
                    v-on:close="close"> </Component>
       </q-scroll-area>
     </div>
