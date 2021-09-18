d77251ec7b49f029cf564db3610a695feb96195f
Peng Zhen <pengzhen@jk.com>
2021-09-18 15:15:32 +0800

    chore: 添加 vetur.config 以支持 mono-repo

---
+++ vetur.config.js
@@ -0,0 +1,19 @@
+// vetur.config.js
+module.exports = {
+  settings: {
+    'vetur.useWorkspaceDependencies': true
+  },
+  projects: [
+    './packages/peace-identity',
+    './packages/peace-library',
+    './packages/peace-ui',
+    './packages/repo-auth',
+    './packages/repo-doctor',
+    './packages/repo-hybrid',
+    './packages/repo-operation',
+    './packages/repo-organization',
+    './packages/repo-patient',
+    './packages/repo-pharmacist',
+    './packages/repo-website'
+  ]
+}
