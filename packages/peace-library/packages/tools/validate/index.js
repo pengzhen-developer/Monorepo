13b4c3d6d96886ab17e647d0ba58449df0780804
周佳 <enxibaba@gmail.com>
2021-09-22 16:08:15 +0800

    fix:修改Libaray身份证校验大小写忽略

--- packages/peace-library/packages/tools/validate/index.js
+++ packages/peace-library/packages/tools/validate/index.js
@@ -151,6 +151,7 @@ export function isTelephone(param) {
  * @returns
  */
 export function isIDCard(code) {
+  code = code.toUpperCase()
   const city = {
     11: '北京',
     12: '天津',
@@ -188,7 +189,7 @@ export function isIDCard(code) {
     82: '澳门',
     91: '国外 '
   }
-  const idCardReg = /^[1-9]\d{5}(19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i // 身份证格式正则表达式
+  const idCardReg = /^[1-9]\d{5}(19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|[X])$/i // 身份证格式正则表达式
 
   let isPass = true // 身份证验证是否通过（true通过、false未通过）
 
