9799d0b3026d1aa3ba3491a13cdaa0f82080f06f
周佳 <enxibaba@gmail.com>
2021-10-13 19:08:16 +0800

    fix: 修改配置文件

--- packages/repo-organization-center/src/App.vue
+++ packages/repo-organization-center/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app"
        class="window-width window-height overflow-hidden">
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
@@ -9,7 +9,8 @@
 export default {
   created() {
     Peace.http.defaults.headers.common['token'] = 'd9cb0c6117884ba5aea5fdfdb9e7d77b'
-  }
+  },
+
 }
 </script>
 
