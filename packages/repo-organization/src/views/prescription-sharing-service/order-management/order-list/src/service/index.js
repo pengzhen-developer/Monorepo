1eb9901ec17adea600d27603b43ccf9b22109347
彭真 <pengz@jzteyao.com>
2021-10-19 14:11:19 +0800

    fix: 修改导出 api 地址

--- packages/repo-organization/src/views/prescription-sharing-service/order-management/order-list/src/service/index.js
+++ packages/repo-organization/src/views/prescription-sharing-service/order-management/order-list/src/service/index.js
@@ -61,7 +61,7 @@ export default {
   exportFile(params) {
     const isMock = false
 
-    const apiPath = 'psd/Excel/OOrderAllField'
+    const apiPath = 'psd/Excel/OOrderAllFieldByCust'
     const mockPath = process.env.VUE_APP_MOCK_API + apiPath
     const serverPath = process.env.VUE_APP_API_BASE + apiPath
 
