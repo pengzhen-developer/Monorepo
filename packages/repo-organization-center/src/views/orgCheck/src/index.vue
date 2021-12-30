e38ad1c8beb2dbaac8dd479d27ebccb273d00f28
周佳 <enxibaba@gmail.com>
2021-12-30 11:06:36 +0800

    fix: 修改审核机构文案

--- packages/repo-organization-center/src/views/orgCheck/src/index.vue
+++ packages/repo-organization-center/src/views/orgCheck/src/index.vue
@@ -177,7 +177,7 @@
 
             <div class="row col q-mb-12">
               <info-item :value="newInstitutionInfoAuditDetails.institutionInfoAuditVO.channelSourceName"
-                         label="应用来源" />
+                         label="来源" />
               <info-item :value="newInstitutionInfoAuditDetails.institutionInfoAuditVO.auditerType | auditerName"
                          label="审核类型" />
               <info-item :value="newInstitutionInfoAuditDetails.institutionInfoAuditVO.updateTime"
